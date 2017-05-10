810898f2215c8898c3f6b148b6626d4e50f9e15d
Will Baumann <wbaumann@github.com>
2017-05-10 18:34:52 -0400

    Fixed a crash in which we'd crash after we'd rotate our cache

--- app/src/main/java/co/smartreceipts/android/utils/cache/FragmentStateCache.java
+++ app/src/main/java/co/smartreceipts/android/utils/cache/FragmentStateCache.java
@@ -1,44 +1,52 @@
 package co.smartreceipts.android.utils.cache;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
 import java.util.HashMap;
 
 import javax.inject.Inject;
 
 import co.smartreceipts.android.di.scopes.ApplicationScope;
+import co.smartreceipts.android.utils.log.Logger;
 
 @ApplicationScope
 public class FragmentStateCache {
 
-    private HashMap<Class<? extends Fragment>, Bundle> argumentsCacheHashMap = new HashMap<>();
-    private HashMap<Class<? extends Fragment>, Bundle> stateCacheHashMap = new HashMap<>();
+    private final HashMap<Class<? extends Fragment>, Bundle> argumentsCacheHashMap = new HashMap<>();
+    private final HashMap<Class<? extends Fragment>, Bundle> stateCacheHashMap = new HashMap<>();
 
     @Inject
     public FragmentStateCache() {
     }
 
-    public Bundle getArguments(Class<? extends Fragment> fragmentClass) {
+    public Bundle getArguments(@NonNull Class<? extends Fragment> fragmentClass) {
         return argumentsCacheHashMap.get(fragmentClass);
     }
 
-    public void putArguments(Bundle args, Class<? extends Fragment> fragmentClass) {
+    public void putArguments(@NonNull Bundle args, @NonNull Class<? extends Fragment> fragmentClass) {
         argumentsCacheHashMap.put(fragmentClass, args);
     }
 
-    public Bundle getSavedState(Class<? extends Fragment> fragmentClass) {
+    public Bundle getSavedState(@NonNull Class<? extends Fragment> fragmentClass) {
         return stateCacheHashMap.get(fragmentClass);
     }
 
-    public void putSavedState(Bundle state, Class<? extends Fragment> fragmentClass) {
+    public void putSavedState(@NonNull Bundle state, @NonNull Class<? extends Fragment> fragmentClass) {
         stateCacheHashMap.put(fragmentClass, state);
     }
 
-    public void onDestroy(Fragment fragment) {
-        // clear cache if fragment is not going to be recreated
-        if (fragment.getActivity() != null && !fragment.getActivity().isChangingConfigurations()) {
-            remove(fragment.getClass());
+    public void onDestroy(@NonNull Fragment fragment) {
+        final FragmentActivity activity = fragment.getActivity();
+        if (activity != null && !activity.isChangingConfigurations()) {
+            if (activity.isDestroyed() && !activity.isFinishing()) {
+                Logger.debug(this, "{}'s activity was released (but not destroyed) by Android. Retaining our cache", fragment.getClass());
+            } else {
+                Logger.debug(this, "Removing {} from the cache as it is being fully destroyed", fragment.getClass());
+                remove(fragment.getClass());
+            }
         }
     }
 
