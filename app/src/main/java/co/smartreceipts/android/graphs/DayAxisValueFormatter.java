38f12ac4eb2865884915b0071be4141995d83757
Julia <julia.soboleva26@gmail.com>
2017-07-03 18:04:09 +0300

    Dates now look like MM/DD for US and DD/MM for others locales

--- app/src/main/java/co/smartreceipts/android/graphs/DayAxisValueFormatter.java
+++ app/src/main/java/co/smartreceipts/android/graphs/DayAxisValueFormatter.java
@@ -4,9 +4,9 @@ import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.util.Calendar;
+import java.util.Locale;
 
-public class DayAxisValueFormatter implements IAxisValueFormatter
-{
+public class DayAxisValueFormatter implements IAxisValueFormatter {
     private final String defaultSeparator;
 
     public DayAxisValueFormatter(String dafaultSeparator) {
@@ -16,8 +16,7 @@ public class DayAxisValueFormatter implements IAxisValueFormatter
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
 
-        // Hack for sometimes appearing IndexOutOfBoundsException from MPAndroidCharts lib
-        try {
+        try { // Hack for sometimes appearing IndexOutOfBoundsException from MPAndroidCharts lib
             int days = (int) value;
 
             Calendar calendar = Calendar.getInstance();
@@ -26,7 +25,11 @@ public class DayAxisValueFormatter implements IAxisValueFormatter
             int day = calendar.get(Calendar.DAY_OF_MONTH);
             int month = calendar.get(Calendar.MONTH) + 1;
 
-            return String.format("%02d", day) + defaultSeparator + String.format("%02d", month);
+            if (Locale.getDefault() == Locale.US) {
+                return String.format("%02d", month) + defaultSeparator + String.format("%02d", day);
+            } else {
+                return String.format("%02d", day) + defaultSeparator + String.format("%02d", month);
+            }
         } catch (IndexOutOfBoundsException e) {
             return "";
         }
