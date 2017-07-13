0152137877d765fe1f6f48a0da7f4288014a8fa0
Julia <julia.soboleva26@gmail.com>
2017-07-14 00:43:14 +0300

    Attempt to fix StackOverflow error

--- app/src/main/java/co/smartreceipts/android/trips/TripFragmentPresenter.java
+++ app/src/main/java/co/smartreceipts/android/trips/TripFragmentPresenter.java
@@ -1,10 +1,13 @@
 package co.smartreceipts.android.trips;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 
 import co.smartreceipts.android.di.scopes.FragmentScope;
 import co.smartreceipts.android.rating.AppRatingManager;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import wb.android.storage.StorageManager;
 
 @FragmentScope
@@ -17,18 +20,25 @@ public class TripFragmentPresenter {
     @Inject
     StorageManager storageManager;
 
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     @Inject
     public TripFragmentPresenter() {
     }
 
-    public void checkRating() {
-        appRatingManager.checkIfNeedToAskRating()
+    public void unsubscribe() {
+        compositeDisposable.clear();
+    }
+
+    public void subscribe() {
+        compositeDisposable.add(appRatingManager.checkIfNeedToAskRating()
+                .delay(3, TimeUnit.SECONDS) // <-- this magic line resolves StackOverflow error
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(ratingPrompt -> {
                     if (ratingPrompt) {
                         fragment.showRatingTooltip();
                     }
-                });
+                }));
     }
 
     public void dontShowRatingPrompt() {
