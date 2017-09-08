d3b88b3232dcea26518a214d7d1d50ffd1efa91f
Will Baumann <wbaumann@github.com>
2017-09-08 16:38:56 -0400

    Deleted one-off ad provider

--- app/src/free/java/co/smartreceipts/android/ad/provider/DefaultAdProvider.java
+++ app/src/free/java/co/smartreceipts/android/ad/provider/DefaultAdProvider.java
@@ -10,7 +10,6 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 
 import co.smartreceipts.android.ad.AdPresenter;
-import co.smartreceipts.android.ad.abcmouse.AbcMouseAdPresenter;
 import co.smartreceipts.android.ad.admob.presenter.ClassicBannerAdPresenter;
 import co.smartreceipts.android.ad.admob.presenter.NativeBannerAdPresenter;
 import co.smartreceipts.android.ad.markets.MarketsComAdPresenter;
@@ -23,12 +22,10 @@ public class DefaultAdProvider implements Provider<AdPresenter> {
 
     private static final int RANDOM_MAX = 100;
     private static final int MARKETS_AD_FREQUENCY = 5; // Out of 100
-    private static final int ABC_MOUSE_AD_FREQUENCY = 1; // Out of 100
 
     private final RegionChecker regionChecker;
     private final Provider<ClassicBannerAdPresenter> classicBannerAdPresenterProvider;
     private final Provider<NativeBannerAdPresenter> nativeBannerAdPresenterProvider;
-    private final Provider<AbcMouseAdPresenter> abcMouseAdPresenterProvider;
     private final Provider<MarketsComAdPresenter> marketsComAdPresenterProvider;
 
     private final Random random = new Random();
@@ -37,12 +34,10 @@ public class DefaultAdProvider implements Provider<AdPresenter> {
     public DefaultAdProvider(@NonNull RegionChecker regionChecker,
                              @NonNull Provider<ClassicBannerAdPresenter> classicBannerAdPresenterProvider,
                              @NonNull Provider<NativeBannerAdPresenter> nativeBannerAdPresenterProvider,
-                             @NonNull Provider<AbcMouseAdPresenter> abcMouseAdPresenterProvider,
                              @NonNull Provider<MarketsComAdPresenter> marketsComAdPresenterProvider) {
         this.regionChecker = Preconditions.checkNotNull(regionChecker);
         this.classicBannerAdPresenterProvider = Preconditions.checkNotNull(classicBannerAdPresenterProvider);
         this.nativeBannerAdPresenterProvider = Preconditions.checkNotNull(nativeBannerAdPresenterProvider);
-        this.abcMouseAdPresenterProvider = Preconditions.checkNotNull(abcMouseAdPresenterProvider);
         this.marketsComAdPresenterProvider = Preconditions.checkNotNull(marketsComAdPresenterProvider);
     }
 
@@ -50,8 +45,6 @@ public class DefaultAdProvider implements Provider<AdPresenter> {
     public AdPresenter get() {
         if (regionChecker.isInWesternEurope() && shouldShowMarketsAd()) {
             return marketsComAdPresenterProvider.get();
-        } else if (shouldShowAbcMouseAd()) {
-            return abcMouseAdPresenterProvider.get();
         } else {
             return FeatureFlags.UseNativeAds.isEnabled() ? nativeBannerAdPresenterProvider.get() : classicBannerAdPresenterProvider.get();
         }
@@ -61,7 +54,4 @@ public class DefaultAdProvider implements Provider<AdPresenter> {
         return MARKETS_AD_FREQUENCY >= random.nextInt(RANDOM_MAX + 1);
     }
 
-    private boolean shouldShowAbcMouseAd() {
-        return ABC_MOUSE_AD_FREQUENCY >= random.nextInt(RANDOM_MAX + 1);
-    }
 }
