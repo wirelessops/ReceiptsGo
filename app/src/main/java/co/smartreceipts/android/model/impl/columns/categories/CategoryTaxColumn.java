3a88571ccd76717ad889fda750a7633b81624853
Will Baumann <wbaumann@github.com>
2017-12-05 16:02:06 -0500

    Added footer support for the grouped categories. Longer term, we should probably just convert these into receipts to take advantage of our existing column framework

--- app/src/main/java/co/smartreceipts/android/model/impl/columns/categories/CategoryTaxColumn.java
+++ app/src/main/java/co/smartreceipts/android/model/impl/columns/categories/CategoryTaxColumn.java
@@ -3,6 +3,12 @@ package co.smartreceipts.android.model.impl.columns.categories;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import co.smartreceipts.android.currency.PriceCurrency;
+import co.smartreceipts.android.model.Price;
+import co.smartreceipts.android.model.factory.PriceBuilderFactory;
 import co.smartreceipts.android.model.impl.columns.AbstractColumnImpl;
 import co.smartreceipts.android.persistence.database.controllers.grouping.results.SumCategoryGroupingResult;
 import co.smartreceipts.android.sync.model.SyncState;
@@ -18,5 +24,20 @@ public class CategoryTaxColumn extends AbstractColumnImpl<SumCategoryGroupingRes
     @Override
     public String getValue(@NonNull SumCategoryGroupingResult sumCategoryGroupingResult) {
         return sumCategoryGroupingResult.getTax().getDecimalFormattedPrice();
+    }
+
+    @NonNull
+    @Override
+    public String getFooter(@NonNull List<SumCategoryGroupingResult> rows) {
+        if (!rows.isEmpty()) {
+            final PriceCurrency tripCurrency = rows.get(0).getCurrency();
+            final List<Price> prices = new ArrayList<>();
+            for (final SumCategoryGroupingResult row : rows) {
+                prices.add(row.getTax());
+            }
+            return new PriceBuilderFactory().setPrices(prices, tripCurrency).build().getDecimalFormattedPrice();
+        } else {
+            return "";
+        }
     }
 }
