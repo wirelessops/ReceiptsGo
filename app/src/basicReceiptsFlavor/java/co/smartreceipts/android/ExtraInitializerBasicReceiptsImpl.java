bb552afdde55ce132436dbc75fc6a64c85ea84e0
Will Baumann <wbaumann@github.com>
2018-05-07 23:00:29 -0400

    Added support for a new basic receipts white-label variant

---
+++ app/src/basicReceiptsFlavor/java/co/smartreceipts/android/ExtraInitializerBasicReceiptsImpl.java
@@ -0,0 +1,17 @@
+package co.smartreceipts.android;
+
+import javax.inject.Inject;
+
+public class ExtraInitializerBasicReceiptsImpl implements ExtraInitializer {
+
+    @Inject
+    public ExtraInitializerBasicReceiptsImpl() {
+    }
+
+
+    @Override
+    public void init() {
+        /* no-op */
+    }
+    
+}
---
+++ app/src/basicReceiptsFlavor/java/co/smartreceipts/android/di/FlavorModule.java
@@ -0,0 +1,39 @@
+package co.smartreceipts.android.di;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import co.smartreceipts.android.ExtraInitializer;
+import co.smartreceipts.android.ExtraInitializerBasicReceiptsImpl;
+import co.smartreceipts.android.analytics.Analytics;
+import co.smartreceipts.android.analytics.AnalyticsManager;
+import co.smartreceipts.android.analytics.impl.firebase.FirebaseAnalytics;
+import co.smartreceipts.android.analytics.impl.logger.AnalyticsLogger;
+import co.smartreceipts.android.di.scopes.ApplicationScope;
+import co.smartreceipts.android.purchases.wallet.PlusPurchaseWallet;
+import co.smartreceipts.android.purchases.wallet.PurchaseWallet;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class FlavorModule {
+
+    @Provides
+    @ApplicationScope
+    public static PurchaseWallet providePurchaseWallet(PlusPurchaseWallet plusPurchaseWallet) {
+        return plusPurchaseWallet;
+    }
+
+    @Provides
+    @ApplicationScope
+    public static ExtraInitializer provideExtraInitializer(ExtraInitializerBasicReceiptsImpl plusInitializer) {
+        return plusInitializer;
+    }
+
+    @Provides
+    @ApplicationScope
+    public static Analytics provideAnalytics(FirebaseAnalytics firebaseAnalytics) {
+        return new AnalyticsManager(Collections.unmodifiableList(Arrays.asList(new AnalyticsLogger(),
+                firebaseAnalytics)));
+    }
+}
