f804664285650b6618fe80b96fdb715edb0d05a8
Will Baumann <wbaumann@github.com>
2018-05-09 15:15:07 -0400

    Removed bug sense as a crash reporter

--- app/src/free/java/co/smartreceipts/android/ExtraInitializerFreeImpl.java
+++ app/src/free/java/co/smartreceipts/android/ExtraInitializerFreeImpl.java
@@ -1,22 +1,9 @@
 package co.smartreceipts.android;
 
-import android.content.Context;
-
-import com.bugsense.trace.BugSenseHandler;
-
-import javax.inject.Inject;
-
-public class ExtraInitializerFreeImpl implements ExtraInitializer{
-
-    @Inject
-    Context context;
-
-    @Inject
-    public ExtraInitializerFreeImpl() {
-    }
+public class ExtraInitializerFreeImpl implements ExtraInitializer {
 
     @Override
     public void init() {
-        BugSenseHandler.initAndStartSession(context, "01de172a");
+        // Intentional no-op
     }
 }
