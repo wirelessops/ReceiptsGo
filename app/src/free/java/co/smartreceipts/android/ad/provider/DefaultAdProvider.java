2f49b4dfd82fe703311436d8552879d26beb6d3a
Will Baumann <wbaumann@github.com>
2018-07-01 19:38:58 -0400

    Removed support for a legacy ad provider

--- app/src/free/java/co/smartreceipts/android/ad/provider/DefaultAdProvider.java
+++ app/src/free/java/co/smartreceipts/android/ad/provider/DefaultAdProvider.java
@@ -11,8 +11,6 @@ import javax.inject.Provider;
 
 import co.smartreceipts.android.ad.AdPresenter;
 import co.smartreceipts.android.ad.admob.presenter.ClassicBannerAdPresenter;
-import co.smartreceipts.android.ad.markets.MarketsComAdPresenter;
-import co.smartreceipts.android.ad.region.RegionChecker;
 import co.smartreceipts.android.di.scopes.ActivityScope;
 
 @ActivityScope
@@ -21,28 +19,18 @@ public class DefaultAdProvider implements Provider<AdPresenter> {
     private static final int RANDOM_MAX = 100;
     private static final int MARKETS_AD_FREQUENCY = 5; // Out of 100
 
-    private final RegionChecker regionChecker;
     private final Provider<ClassicBannerAdPresenter> classicBannerAdPresenterProvider;
-    private final Provider<MarketsComAdPresenter> marketsComAdPresenterProvider;
 
     private final Random random = new Random();
 
     @Inject
-    public DefaultAdProvider(@NonNull RegionChecker regionChecker,
-                             @NonNull Provider<ClassicBannerAdPresenter> classicBannerAdPresenterProvider,
-                             @NonNull Provider<MarketsComAdPresenter> marketsComAdPresenterProvider) {
-        this.regionChecker = Preconditions.checkNotNull(regionChecker);
+    public DefaultAdProvider(@NonNull Provider<ClassicBannerAdPresenter> classicBannerAdPresenterProvider) {
         this.classicBannerAdPresenterProvider = Preconditions.checkNotNull(classicBannerAdPresenterProvider);
-        this.marketsComAdPresenterProvider = Preconditions.checkNotNull(marketsComAdPresenterProvider);
     }
 
     @Override
     public AdPresenter get() {
-        if (regionChecker.isInWesternEurope() && shouldShowMarketsAd()) {
-            return marketsComAdPresenterProvider.get();
-        } else {
-            return classicBannerAdPresenterProvider.get();
-        }
+        return classicBannerAdPresenterProvider.get();
     }
 
     private boolean shouldShowMarketsAd() {
