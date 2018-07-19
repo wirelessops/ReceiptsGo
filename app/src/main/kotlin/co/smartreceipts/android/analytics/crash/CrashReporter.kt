a7e223fa01b8ec402df90fec42a3aced964b4c09
Will Baumann <wbaumann@github.com>
2018-07-19 11:06:28 -0400

    Ensured that Fabric initialization occurs in the background

--- app/src/main/kotlin/co/smartreceipts/android/analytics/crash/CrashReporter.kt
+++ app/src/main/kotlin/co/smartreceipts/android/analytics/crash/CrashReporter.kt
@@ -12,6 +12,8 @@ import co.smartreceipts.android.di.scopes.ApplicationScope
 import co.smartreceipts.android.settings.UserPreferenceManager
 import co.smartreceipts.android.settings.catalog.UserPreference
 import io.fabric.sdk.android.Fabric
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers
 
 @ApplicationScope
 class CrashReporter @Inject constructor(private val context: Context, private val userPreferenceManager: UserPreferenceManager) {
@@ -20,13 +22,16 @@ class CrashReporter @Inject constructor(private val context: Context, private va
      * Initializes our crash reporter to determine if we should track crashes or not
      */
     fun initialize() {
-        // Set up Crashlytics, disabling it when the user has elected to disable the functionality
-        val crashlyticsKit = Crashlytics.Builder()
-                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG || userPreferenceManager.get(UserPreference.Privacy.EnableCrashTracking)).build())
-                .build()
+        Completable.fromCallable {
+                    // Set up Crashlytics, disabling it when the user has elected to disable the functionality
+                    val crashlyticsKit = Crashlytics.Builder()
+                            .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG || userPreferenceManager.get(UserPreference.Privacy.EnableCrashTracking)).build())
+                            .build()
 
-        // Initialize Fabric with the custom crashlytics instance
-        Fabric.with(context, crashlyticsKit)
-
+                    // Initialize Fabric with the custom crashlytics instance
+                    Fabric.with(context, crashlyticsKit)
+                }
+                .subscribeOn(Schedulers.io())
+                .subscribe()
     }
 }
