99ebd11d72d11d0552c124d8d97815a1f3591c5d
Will Baumann <wbaumann@github.com>
2018-07-22 22:19:31 -0400

    Updated our categories and payment method updates to clear the receipts cache on changes

--- app/src/main/java/co/smartreceipts/android/persistence/database/controllers/impl/PaymentMethodsTableController.java
+++ app/src/main/java/co/smartreceipts/android/persistence/database/controllers/impl/PaymentMethodsTableController.java
@@ -6,12 +6,13 @@ import co.smartreceipts.android.analytics.Analytics;
 import co.smartreceipts.android.di.scopes.ApplicationScope;
 import co.smartreceipts.android.model.PaymentMethod;
 import co.smartreceipts.android.persistence.DatabaseHelper;
+import co.smartreceipts.android.persistence.database.controllers.alterations.PaymentMethodsTableActionAlterations;
 
 @ApplicationScope
 public class PaymentMethodsTableController extends AbstractTableController<PaymentMethod> {
 
     @Inject
     public PaymentMethodsTableController(DatabaseHelper databaseHelper, Analytics analytics) {
-        super(databaseHelper.getPaymentMethodsTable(), analytics);
+        super(databaseHelper.getPaymentMethodsTable(), new PaymentMethodsTableActionAlterations(databaseHelper.getReceiptsTable()), analytics);
     }
 }
