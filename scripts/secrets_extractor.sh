146668d55a409ed5dc9a5efe431622e04b50b730
Will Baumann <wbaumann@github.com>
2018-10-01 21:17:08 -0400

    Another attempt to fix the travis builds

--- scripts/secrets_extractor.sh
+++ scripts/secrets_extractor.sh
@@ -1,4 +1,4 @@
-git update-index --assume-unchanged app/src/free/res/values/ads.xml 2> /dev/null
-git update-index --assume-unchanged app/src/free/res/xml/analytics.xml 2> /dev/null
-git update-index --assume-unchanged app/src/main/res/values/secrets.xml 2> /dev/null
-gpg -d secrets.tar.gpg | tar xv 2> /dev/null
+git update-index --assume-unchanged app/src/free/res/values/ads.xml
+git update-index --assume-unchanged app/src/free/res/xml/analytics.xml
+git update-index --assume-unchanged app/src/main/res/values/secrets.xml
+gpg -d secrets.tar.gpg | tar xv || true
