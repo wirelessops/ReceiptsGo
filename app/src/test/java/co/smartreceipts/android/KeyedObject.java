c21439dc988de6a119c6df382cab1a1aa0bfcbc4
Will Baumann <wbaumann@github.com>
2018-10-07 19:10:59 -0400

    Fixed a bug that prevented our tests from running properly

--- app/src/test/java/co/smartreceipts/android/KeyedObject.java
+++ app/src/test/java/co/smartreceipts/android/KeyedObject.java
@@ -7,18 +7,27 @@ import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
 import co.smartreceipts.android.model.Keyed;
+import co.smartreceipts.android.sync.model.SyncState;
+import co.smartreceipts.android.sync.model.Syncable;
+import co.smartreceipts.android.sync.model.impl.DefaultSyncState;
 
 @VisibleForTesting
-public class KeyedObject implements Keyed {
+public class KeyedObject implements Keyed, Syncable {
 
-        @Override
-        public int getId() {
-            return Keyed.MISSING_ID;
-        }
-
-        @NotNull
-        @Override
-        public UUID getUuid() {
-            return Keyed.Companion.getMISSING_UUID();
-        }
+    @Override
+    public int getId() {
+        return Keyed.MISSING_ID;
     }
+
+    @NotNull
+    @Override
+    public UUID getUuid() {
+        return Keyed.Companion.getMISSING_UUID();
+    }
+
+    @NotNull
+    @Override
+    public SyncState getSyncState() {
+        return new DefaultSyncState();
+    }
+}
