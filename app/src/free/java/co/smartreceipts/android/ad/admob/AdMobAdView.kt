3d33a6ae57954b039769e13e474c2b90efe923ed
Will Baumann <wbaumann@github.com>
2019-01-06 19:34:42 -0500

    Added hints to the report inpur fields
    This should hopefully better clarify the intention of each to new users

--- app/src/free/java/co/smartreceipts/android/ad/admob/AdMobAdView.kt
+++ app/src/free/java/co/smartreceipts/android/ad/admob/AdMobAdView.kt
@@ -96,6 +96,7 @@ class AdMobAdView @Inject constructor() : BannerAdView {
         private fun getAdRequest(allowAdPersonalization: Boolean): AdRequest {
             val builder = AdRequest.Builder()
                     .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .addTestDevice("EF80F770CD5E7AA481D559EC1037AE2D")
                     .addTestDevice("BFB48A3556EED9C87CB3AD907780D610")
                     .addTestDevice("E03AEBCB2894909B8E4EC87C0368C242")
                     .addTestDevice("B48FF89819FAB2B50FE3E5240FCD9741")
