a4733bdc3898dd1fc6cbed6e9555953b95da6049
Julia Soboleva <julia.soboleva26@gmail.com>
2019-02-14 22:13:31 +0200

    Converted OcrConfigurationPresenter and OcrConfigurationInteractor to Kotlin

---
+++ app/src/main/java/co/smartreceipts/android/ocr/widget/configuration/OcrConfigurationInteractor.kt
@@ -0,0 +1,88 @@
+package co.smartreceipts.android.ocr.widget.configuration
+
+import co.smartreceipts.android.analytics.Analytics
+import co.smartreceipts.android.analytics.events.DataPoint
+import co.smartreceipts.android.analytics.events.DefaultDataPointEvent
+import co.smartreceipts.android.analytics.events.Events
+import co.smartreceipts.android.di.scopes.FragmentScope
+import co.smartreceipts.android.identity.IdentityManager
+import co.smartreceipts.android.identity.store.EmailAddress
+import co.smartreceipts.android.ocr.purchases.OcrPurchaseTracker
+import co.smartreceipts.android.purchases.PurchaseManager
+import co.smartreceipts.android.purchases.model.AvailablePurchase
+import co.smartreceipts.android.purchases.model.ConsumablePurchase
+import co.smartreceipts.android.purchases.model.InAppPurchase
+import co.smartreceipts.android.purchases.model.PurchaseFamily
+import co.smartreceipts.android.purchases.source.PurchaseSource
+import co.smartreceipts.android.settings.UserPreferenceManager
+import co.smartreceipts.android.settings.catalog.UserPreference
+import co.smartreceipts.android.utils.log.Logger
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.math.BigDecimal
+import javax.inject.Inject
+
+@FragmentScope
+class OcrConfigurationInteractor @Inject
+constructor(
+    private val identityManager: IdentityManager,
+    private val ocrPurchaseTracker: OcrPurchaseTracker,
+    private val purchaseManager: PurchaseManager,
+    private val userPreferenceManager: UserPreferenceManager,
+    private val analytics: Analytics
+) {
+
+    val email: EmailAddress?
+        get() = identityManager.email
+
+    val isUserLoggedIn: Boolean
+        get() = identityManager.isLoggedIn
+
+
+    fun getRemainingScansStream(): Observable<Int> {
+        return ocrPurchaseTracker.remainingScansStream
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun getAvailableOcrPurchases(): Single<List<AvailablePurchase>> {
+        return purchaseManager.allAvailablePurchases
+            .flatMapIterable { availablePurchases -> availablePurchases }
+            .filter { availablePurchase ->
+                availablePurchase.getInAppPurchase() != null && availablePurchase.getInAppPurchase()!!.type == ConsumablePurchase::class.java
+                        && availablePurchase.getInAppPurchase()!!.purchaseFamilies.contains(PurchaseFamily.Ocr)
+            }
+            .toSortedList { purchase1, purchase2 ->
+                BigDecimal.valueOf(purchase1.priceAmountMicros).compareTo(BigDecimal.valueOf(purchase2.priceAmountMicros))
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    fun getOcrIsEnabled(): Observable<Boolean> = userPreferenceManager.getObservable(UserPreference.Misc.OcrIsEnabled)
+
+    fun getAllowUsToSaveImagesRemotely(): Observable<Boolean> {
+        return userPreferenceManager.getObservable(UserPreference.Misc.OcrIncognitoMode)
+            .map { incognito -> !incognito }
+    }
+
+
+    fun startOcrPurchase(inAppPurchase: InAppPurchase?) {
+        if (inAppPurchase != null) {
+            analytics.record(DefaultDataPointEvent(Events.Ocr.OcrPurchaseClicked).addDataPoint(DataPoint("sku", inAppPurchase)))
+            purchaseManager.initiatePurchase(inAppPurchase, PurchaseSource.Ocr)
+        } else {
+            Logger.error(this, "Unexpected state in which the in app purchase is null")
+        }
+    }
+
+    fun setOcrIsEnabled(ocrIsEnabled: Boolean) {
+        analytics.record(DefaultDataPointEvent(Events.Ocr.OcrIsEnabledToggled).addDataPoint(DataPoint("value", ocrIsEnabled)))
+        userPreferenceManager[UserPreference.Misc.OcrIsEnabled] = ocrIsEnabled
+    }
+
+    fun setAllowUsToSaveImagesRemotely(saveImagesRemotely: Boolean) {
+        analytics.record(DefaultDataPointEvent(Events.Ocr.OcrIncognitoModeToggled).addDataPoint(DataPoint("value", !saveImagesRemotely)))
+        userPreferenceManager[UserPreference.Misc.OcrIncognitoMode] = !saveImagesRemotely
+    }
+
+}
---
+++ app/src/main/java/co/smartreceipts/android/ocr/widget/configuration/OcrConfigurationPresenter.kt
@@ -0,0 +1,68 @@
+package co.smartreceipts.android.ocr.widget.configuration
+
+import co.smartreceipts.android.di.scopes.FragmentScope
+import co.smartreceipts.android.purchases.model.InAppPurchase
+import co.smartreceipts.android.utils.log.Logger
+import co.smartreceipts.android.widget.viper.BaseViperPresenter
+import javax.inject.Inject
+
+@FragmentScope
+class OcrConfigurationPresenter @Inject
+constructor(view: OcrConfigurationView, interactor: OcrConfigurationInteractor) :
+    BaseViperPresenter<OcrConfigurationView, OcrConfigurationInteractor>(view, interactor) {
+
+    override fun subscribe() {
+        view.present(interactor.email)
+
+        // Set the current checkbox values
+        compositeDisposable.add(
+            interactor.getOcrIsEnabled()
+                .subscribe(view.ocrIsEnabledConsumer)
+        )
+        compositeDisposable.add(
+            interactor.getAllowUsToSaveImagesRemotely()
+                .subscribe(view.allowUsToSaveImagesRemotelyConsumer)
+        )
+
+        // Persist values from checkbox toggling
+        compositeDisposable.add(view.ocrIsEnabledCheckboxChanged
+            .doOnNext { Logger.debug(this, "Updating ocrIsEnabled setting: {}", it) }
+            .subscribe { interactor.setOcrIsEnabled(it) }
+        )
+        compositeDisposable.add(view.allowUsToSaveImagesRemotelyCheckboxChanged
+            .doOnNext { Logger.debug(this, "Updating saveImagesRemotely setting: {}", it) }
+            .subscribe { interactor.setAllowUsToSaveImagesRemotely(it) }
+        )
+
+        // Show remaining scans
+        compositeDisposable.add(interactor.getRemainingScansStream()
+            .subscribe { view.present(it) }
+        )
+
+        // Show available purchases list
+        compositeDisposable.add(interactor.getAvailableOcrPurchases()
+            .doOnSuccess { Logger.info(this, "Presenting list of purchases: {}.", it) }
+            .subscribe({ view.present(it) },
+                { throwable -> Logger.warn(this, "Failed to get available purchases.", throwable) })
+        )
+
+        // Track user purchase clicks
+        compositeDisposable.add(view.availablePurchaseClicks
+            .doOnNext { Logger.info(this, "User clicked to buy purchase: {}.", it) }
+            .subscribe { availablePurchase ->
+                if (interactor.isUserLoggedIn) {
+                    interactor.startOcrPurchase(availablePurchase.getInAppPurchase())
+                } else {
+                    view.delayPurchaseAndPresentNeedToLogin(availablePurchase.getInAppPurchase()!!.sku)
+                }
+            })
+
+        compositeDisposable.add(view.delayedPurchaseIdStream
+            .subscribe { purchaseId ->
+                if (interactor.isUserLoggedIn) {
+                    interactor.startOcrPurchase(InAppPurchase.from(purchaseId))
+                }
+            })
+    }
+
+}
