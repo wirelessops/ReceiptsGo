d51a980577bb512176e8b3d216ae2daad3fcbd65
Steve <ministeve412@gmail.com>
2019-09-30 06:27:54 -0700

    Updating formattable string to include report gross total (#170)
    * updating constructor to include receipts and distances
    
    * updating constructor to include receipts and distances
    
    * adding distances to createReceiptsImagesSection
    
    * adding receipts and distances to writeFile method
    
    * updating report gross total logic
    
    * updating tests to reflect formattable string changes

--- app/src/main/java/co/smartreceipts/android/workers/reports/formatting/SmartReceiptsFormattableString.java
+++ app/src/main/java/co/smartreceipts/android/workers/reports/formatting/SmartReceiptsFormattableString.java
@@ -4,10 +4,15 @@ import androidx.annotation.NonNull;
 
 import com.google.common.base.Preconditions;
 
+import java.util.List;
+
 import co.smartreceipts.android.date.DateFormatter;
+import co.smartreceipts.android.model.Distance;
+import co.smartreceipts.android.model.Receipt;
 import co.smartreceipts.android.model.Trip;
 import co.smartreceipts.android.settings.UserPreferenceManager;
 import co.smartreceipts.android.settings.catalog.UserPreference;
+import co.smartreceipts.android.workers.reports.pdf.pdfbox.ReceiptsTotals;
 
 public class SmartReceiptsFormattableString {
 
@@ -15,20 +20,25 @@ public class SmartReceiptsFormattableString {
     private static final String USER_ID = "%USER_ID%";
     private static final String REPORT_START = "%REPORT_START%";
     private static final String REPORT_END = "%REPORT_END%";
+    private static final String REPORT_GROSS_TOTAL = "%REPORT_GROSS_TOTAL%";
 
     private final String string;
     private final Trip trip;
     private final UserPreferenceManager preferences;
     private final DateFormatter dateFormatter;
+    private final ReceiptsTotals totals;
 
     public SmartReceiptsFormattableString(@NonNull String string,
                                           @NonNull Trip trip,
                                           @NonNull UserPreferenceManager preferences,
-                                          @NonNull DateFormatter dateFormatter) {
+                                          @NonNull DateFormatter dateFormatter,
+                                          @NonNull List<Receipt> receipts,
+                                          @NonNull List<Distance> distances) {
         this.string = Preconditions.checkNotNull(string);
         this.trip = Preconditions.checkNotNull(trip);
         this.preferences = Preconditions.checkNotNull(preferences);
         this.dateFormatter = Preconditions.checkNotNull(dateFormatter);
+        this.totals = new ReceiptsTotals(trip, Preconditions.checkNotNull(receipts), Preconditions.checkNotNull(distances), preferences);
     }
 
     @Override
@@ -37,6 +47,7 @@ public class SmartReceiptsFormattableString {
                 .replace(REPORT_NAME, trip.getName())
                 .replace(USER_ID, preferences.get(UserPreference.ReportOutput.UserId))
                 .replace(REPORT_START, dateFormatter.getFormattedDate(trip.getStartDisplayableDate()))
-                .replace(REPORT_END, dateFormatter.getFormattedDate(trip.getEndDisplayableDate()));
+                .replace(REPORT_END, dateFormatter.getFormattedDate(trip.getEndDisplayableDate()))
+                .replace(REPORT_GROSS_TOTAL, totals.getGrandTotalPrice().getCurrencyFormattedPrice());
     }
 }
