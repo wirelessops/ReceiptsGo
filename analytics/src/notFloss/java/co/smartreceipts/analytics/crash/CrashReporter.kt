bdb5a384cd309c30af6f97c8ea93d26770c402e6
Steve <ministeve412@gmail.com>
2020-06-22 00:02:10 -0700

    Crashlytics updates (#225)
    * updating logic to apply to items where fields are already filled
    
    * removing editor from presenter constructor
    
    * updating tests to check same values whether editing or not
    
    * updating logic to apply to items where fields are already filled
    
    * removing editor from presenter constructor
    
    * updating tests to check same values whether editing or not
    
    * initializing firebaseapp before crashlytics
    
    * adding analytics dependency for breadcrumbs and details
    
    * updating when to enable crashlytics
    
    * updating crashlytics version
    
    * updating firebase messaging version
    
    * moving firebase app initialization to nonfloss analytics constructor

--- analytics/src/notFloss/java/co/smartreceipts/analytics/crash/CrashReporter.kt
+++ analytics/src/notFloss/java/co/smartreceipts/analytics/crash/CrashReporter.kt
@@ -1,15 +1,25 @@
 package co.smartreceipts.analytics.crash
 
+import android.content.Context
+import co.smartreceipts.analytics.BuildConfig
+import com.google.firebase.FirebaseApp
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import javax.inject.Inject
 
-class CrashReporter @Inject constructor() : CrashReporterInterface {
+class CrashReporter @Inject constructor(private val context: Context) : CrashReporterInterface {
 
     /**
      * Initializes our crash reporter to determine if we should track crashes or not
      */
     override fun initialize(isCrashTrackingEnabled: Boolean) {
+        // must be initialized before using Firebase objects
+        FirebaseApp.initializeApp(context)
+
         // Set up Crashlytics, disabling it when the user has elected to disable the functionality
-        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(isCrashTrackingEnabled)
+        if (!BuildConfig.DEBUG) {
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(isCrashTrackingEnabled)
+        } else {
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+        }
     }
 }
