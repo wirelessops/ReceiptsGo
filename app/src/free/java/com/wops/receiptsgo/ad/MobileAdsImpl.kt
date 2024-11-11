b56f9e12e2115aba341507c4283629ea682a996e
wirelessops <9007028+wirelessops@users.noreply.github.com>
2024-11-11 02:06:29 +0100

    Change device hashes for AdMob test

--- app/src/free/java/com/wops/receiptsgo/ad/MobileAdsImpl.kt
+++ app/src/free/java/com/wops/receiptsgo/ad/MobileAdsImpl.kt
@@ -17,12 +17,7 @@ class MobileAdsImpl @Inject constructor(private val context: Context) : MobileAd
                 .setTestDeviceIds(
                     listOf(
                         AdRequest.DEVICE_ID_EMULATOR,
-                        "EF80F770CD5E7AA481D559EC1037AE2D",
-                        "BFB48A3556EED9C87CB3AD907780D610",
-                        "E03AEBCB2894909B8E4EC87C0368C242",
-                        "B48FF89819FAB2B50FE3E5240FCD9741",
-                        "F868E3E348ACF850C6454323A90E2F09",
-                        "E5709A4C156B990EFD896EC4719AA8F0"
+                        "152286E4E1351DECFB54AAD85E9FB5E1"
                     )
                 )
                 .build()
