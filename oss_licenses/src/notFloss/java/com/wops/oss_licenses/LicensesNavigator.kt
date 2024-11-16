26ea59bd27ae63e896734600d87265f698960785
wirelessops <9007028+wirelessops@users.noreply.github.com>
2024-11-16 08:37:45 +0100

    Refactor Groovy, disable OSS License Plugin

--- oss_licenses/src/notFloss/java/com/wops/oss_licenses/LicensesNavigator.kt
+++ oss_licenses/src/notFloss/java/com/wops/oss_licenses/LicensesNavigator.kt
@@ -3,15 +3,16 @@ package com.wops.oss_licenses
 import android.content.Context
 import android.content.Intent
 import com.wops.core.di.scopes.ApplicationScope
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+//import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import javax.inject.Inject
 
 @ApplicationScope
 class LicensesNavigator @Inject constructor() : LicensesNavigatorInterface {
 
-    override fun getLicensesActivityIntent(context: Context, ossActivityTitleId: Int): Intent? {
+   override fun getLicensesActivityIntent(context: Context, ossActivityTitleId: Int): Intent? {
 
-        OssLicensesMenuActivity.setActivityTitle(context.getString(ossActivityTitleId))
-        return Intent(context, OssLicensesMenuActivity::class.java)
-    }
+//        OssLicensesMenuActivity.setActivityTitle(context.getString(ossActivityTitleId))
+//        return Intent(context, OssLicensesMenuActivity::class.java)
+       return null
+   }
 }
