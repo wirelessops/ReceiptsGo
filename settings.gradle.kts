b66bb279c516483074979c483d7b49cc69299245
wirelessops <9007028+wirelessops@users.noreply.github.com>
2024-11-27 07:48:34 +0100

    Fix and Re-enable OSS Licenses Plugin
    Force OSS Licenses Plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
 pluginManagement {
-
+    resolutionStrategy {
+        eachPlugin {
+            // Force resolution due to missing Plugin Marker Artifact with the coordinates plugin.id:plugin.id.gradle.plugin:plugin.version
+            if (requested.id.id == "com.google.android.gms.oss-licenses-plugin") {
+                useModule("com.google.android.gms:oss-licenses-plugin:0.10.6")
+            }
+        }
+    }
     repositories {
         google()
         maven(url = "https://maven.google.com")
