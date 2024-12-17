f6580775affb442c34d047ff01e06d374cae60ac
wirelessops <9007028+wirelessops@users.noreply.github.com>
2024-12-17 20:22:12 +0100

    Disable AdModule

--- app/src/free/java/com/wops/receiptsgo/di/ReceiptsGoActivityAdModule.java
+++ app/src/free/java/com/wops/receiptsgo/di/ReceiptsGoActivityAdModule.java
@@ -3,6 +3,8 @@ package com.wops.receiptsgo.di;
 
 import com.wops.receiptsgo.ad.AdPresenter;
 import com.wops.receiptsgo.ad.BannerAdPresenter;
+import com.wops.receiptsgo.ad.EmptyBannerAdPresenter;
+import com.wops.receiptsgo.ad.EmptyInterstitialAdPresenter;
 import com.wops.receiptsgo.ad.InterstitialAdPresenter;
 import com.wops.core.di.scopes.ActivityScope;
 import dagger.Module;
@@ -13,13 +15,13 @@ public class ReceiptsGoActivityAdModule {
 
     @Provides
     @ActivityScope
-    public static AdPresenter provideAdPresenter(BannerAdPresenter bannerAdPresenter) {
+    public static AdPresenter provideAdPresenter(EmptyBannerAdPresenter bannerAdPresenter) {
         return bannerAdPresenter;
     }
 
     @Provides
     @ActivityScope
-    public static InterstitialAdPresenter provideInterstitialAdPresenter(AdMobInterstitialAdPresenter presenter) {
+    public static InterstitialAdPresenter provideInterstitialAdPresenter(EmptyInterstitialAdPresenter presenter) {
         return presenter;
     }
 
