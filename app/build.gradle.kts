1dfcf4625d19693294bb73610b4d7b270dd8da18
wirelessops <9007028+wirelessops@users.noreply.github.com>
2024-12-22 04:23:16 +0100

    Update version to 1.2.4 (1024)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1023
+        versionCode = 1024
         versionName = "1.2.4"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
